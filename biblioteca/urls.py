1f01e5fd72ca7128767fbf1b8438821a159449ca
wellington90 <siswellingtonsantos@gmail.com>
2024-06-24 16:44:34 -0300

    Uma função lambda é usada para redirecionar o URL raiz ( /) para a loginvisualização.

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('livro/', include('livro.urls')),
-    path('auth/', include('usuarios.urls'))
+    path('auth/', include('usuarios.urls')),
+    path('', lambda request: redirect('login')),  # Redirect root URL to the login page
 ]
